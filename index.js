bc968e94960a05cac42928b706fd433bd99d480b
Ranjeet Kumar Yadav <138971007+Ranjeet5644@users.noreply.github.com>
2024-05-08 23:16:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log(" I am Conditionals tutorial!")
+
+let age = 1;
+let grace = 2
+
+ // age += grace
+//console.log(age)
+
+//console.log(age+grace)
+//console.log(age-grace)
+//console.log(age*grace)
+//console.log(age/grace)
+//console.log(age**grace)
+//console.log(age%grace)
+ 
+
+if(age>18){
+
+
+    console.log("You can drive")
+
+
+} 
+else if(age==0){
+
+console.log("Are you kidding")
+
+}
+else if(age==1){
+
+    console.log("Are you again kidding")
+    
+    }
+else{
+
+   console.log("you can not drive");
+
+}
+
+ a = 6;
+ b = 8;
+ let c = a>b ? (a-b):(b-a);
+
+ console.log(c)
